5ac47c571a02df43dde3cf3381fd1315910364e2
Daniel Nieto <danielnieto152326@correo.itm.edu.co>
2022-08-16 11:31:20 -0500

    Final codes march 2022

--- Wavelets/ckaweightedMKL1.py
+++ Wavelets/ckaweightedMKL1.py
@@ -29,7 +29,8 @@ def cvxopt_solve_qp(P, q, G=None, h=None, A=None, b=None):
     return np.array(sol['x']).reshape((P.shape[1],))
 
 def ckaweightedK(K,l):
-    L = np.matmul(l,l.T)
+    L = l*l.T
+#     print(L)
 
     P = K.shape[0]
     N = K.shape[2]
@@ -38,16 +39,17 @@ def ckaweightedK(K,l):
     U = np.eye(N) - (np.matmul(np.ones([N,1]) , np.ones([1,N])))*(1/N)
 
     #Centering the Labels kernel
+    print(U.shape, L.shape)
     L = np.matmul(np.matmul(U,L),U)
 
     #Centering the features kernels
     Kcc = np.zeros_like(K,dtype = float)
     a = np.zeros(P,dtype = float)
-    aux3 = np.trace(np.matmul(L,L))
+    aux3 = np.trace(np.asmatrix((L*L.T)))
     for i in range(P):
         Kcc[i,:,:] = np.matmul(np.matmul(U,np.squeeze(K[i,:,:])),U)
-        aux1 = np.trace(np.matmul(np.squeeze(Kcc[i,:,:]),L))
-        aux2 = np.trace(np.matmul(np.squeeze(Kcc[i,:,:]),np.squeeze(Kcc[i,:,:])))
+        aux1 = np.trace(np.asmatrix(np.squeeze(Kcc[i,:,:])*L))
+        aux2 = np.trace(np.asmatrix(np.squeeze(Kcc[i,:,:])*np.squeeze(Kcc[i,:,:])))
         a[i] = aux1 / np.sqrt(aux2*aux3)
  
 
@@ -55,21 +57,23 @@ def ckaweightedK(K,l):
     M = np.eye(P,dtype=float)
     for i in range(P):
         for j in range(i+1,P):
-            aux = np.trace(np.matmul(np.squeeze(K[i,:,:]),np.squeeze(K[j,:,:])))
-            aux = aux / np.sqrt(np.trace(np.matmul(np.squeeze(K[i,:,:]),np.squeeze(K[i,:,:]))) * np.trace(np.matmul(np.squeeze(K[j,:,:]),np.squeeze(K[j,:,:]))))
+            aux = np.trace(np.asmatrix(np.squeeze(K[i,:,:])*np.squeeze(K[j,:,:])))
+            aux = aux / np.sqrt(np.trace(np.asmatrix(np.squeeze(K[i,:,:])*np.squeeze(K[i,:,:]))) * np.trace(np.asmatrix(np.squeeze(K[j,:,:])*np.squeeze(K[j,:,:]))))
             M[i,j] = aux
             M[j,i] = aux
 
-
+    # Solving the analytic equation
+    eta = sp.linalg.solve(M,a)
+    eta = eta/np.linalg.norm(eta,2)
     #Solving the quadratic optimization problem        
-    A = np.ones(P)
-    A = matrix(A,(1,P),'d')
-    b = matrix(1,(1,1),'d')
-    G = -np.eye(P)
-    h = np.zeros(P)
-    w = cvxopt_solve_qp(P = 2*M, q = -2*a, G = G, h = h, A = A, b = b)
-    eta = w / np.linalg.norm(w)
-    return w
+#     A = np.ones(P)
+#     A = matrix(A,(1,P),'d')
+#     b = matrix(1,(1,1),'d')
+#     G = -np.eye(P)
+#     h = np.zeros(P)
+#     w = cvxopt_solve_qp(P = 2*M, q = -2*a, G = G, h = h, A = A, b = b)
+#     eta = w / np.linalg.norm(w)
+    return eta
 
 
 # In[4]:
@@ -79,18 +83,14 @@ def ckaweightedK(K,l):
 #Inputs: A np array of size (NxNxP) where N is the number of features and P is the number of Kernels
 #L: a vector of labels for creating the "label kernel yy'"
 
-Ko = np.random.rand(10,39,39)
-K = np.zeros_like(Ko)
-for i in range(K.shape[0]):
-    K[i,:,:] = np.matmul(np.squeeze(Ko[i,:,:]),np.squeeze(Ko[i,:,:]).T)
+# Ko = np.random.rand(10,39,39)
+# K = np.zeros_like(Ko)
+# for i in range(K.shape[0]):
+#     K[i,:,:] = np.matmul(np.squeeze(Ko[i,:,:]),np.squeeze(Ko[i,:,:]).T)
     
-l = np.ones([39,1])
-l[0:20] = -1
-print(K.shape,l.shape)
-w = ckaweightedK(K,l)
-
-
-# In[ ]:
+# l = np.ones([39,1])
+# l[0:20] = -1
+# print(K.shape,l.shape)
 
 
 
